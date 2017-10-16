616dc7ad53e729cd5ead4b60a60f4e4b792e7e94
kevintoomey <toomeykev@gmail.com>
2017-10-16 16:41:29 +0200

    structure pour le rendu 21, livrable_21_render

---
+++ src/client/fonctions_livrables/livrable_21_render.cpp
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include <iostream>
+using namespace std;
+
+void livrable_21_render(string commande){
+    if (commande=="render"){
+        cout<<"la commande est render"<<endl;
+    }
+    else{
+        cout<< "la commande n'est pas render"<< endl; 
+    }
+}
--- src/client/main.cpp
+++ src/client/main.cpp
@@ -8,18 +8,15 @@ using namespace std;
 using namespace state;
 void livrable_11_hello(string commande);
 void livrable_1final_state(string commande);
+void livrable_21_render(string commande);
 
 int main(int argc,char* argv[]) 
 {
     if (argc>1){
-        string str(argv[1]);
-        livrable_11_hello(str);
+        string str(argv[1]); // commande entrée lors de l'execution
+        livrable_11_hello(str); // Appelle aux fonctions gérant les différents livrables
         livrable_1final_state(str);
-        if (str=="render")
-        {
-           cout<<"La commande est render"<<endl;
-        }
-        else{cout<<"La commande n'est pas render"<<endl;}
+        livrable_21_render(str);
     }
     else {cout<<"Il n'y a pas de commande"<<endl;}
     
