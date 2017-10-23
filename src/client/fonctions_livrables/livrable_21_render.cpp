59b4ab73126bc776165f33e86776d40d87d49828
Chloé Lodolo <chloe.lodolo@gmail.com>
2017-10-23 17:19:54 +0200

    Changement des ressources et code du livrable 2.1

--- src/client/fonctions_livrables/livrable_21_render.cpp
+++ src/client/fonctions_livrables/livrable_21_render.cpp
@@ -5,11 +5,49 @@
  */
 
 #include <iostream>
-using namespace std;
+#include <SFML/Graphics.hpp>
+using namespace sf;
 
-void livrable_21_render(string commande){
+void livrable_21_render(std::string commande){
     if (commande=="render"){
-        cout<<"la commande est render"<<endl;
+        std::cout<<"la commande est render"<<std::endl;
+        
+        RenderWindow window(VideoMode(800,600,32),"First try", Style::Close | Style::Titlebar);
+        
+        
+        
+        // on fait tourner le programme tant que la fenêtre n'est pas fermée
+        while (window.isOpen())
+        {
+            Event event;
+            while (window.pollEvent(event))
+            {
+                // fermeture de la fenêtre lorsque l'utilisateur le souhaite
+                if (event.type == Event::Closed)
+                {
+                    window.close();
+                }
+            }
+            
+            // à chaque tour, on efface l'ancien rendu
+            window.clear(Color::White);
+            
+            Texture dragonTerritory;
+            if (!dragonTerritory.loadFromFile("res/texture_dragon.png"))
+            {
+                // si ça ne marche pas, on fait une exception
+                throw std::runtime_error("Impossible de lire le fichier");
+            }
+            
+            Sprite tileDragon;
+            tileDragon.setTexture(dragonTerritory);
+            tileDragon.setTextureRect(IntRect(0,200,100,100));
+            
+            // on définit ici tout ce qu'on dessine
+            window.draw(tileDragon);
+            // et on affiche le nouveau rendu
+            window.display();
+        }
         
     }
     else{
