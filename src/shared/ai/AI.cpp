3115f8a547a5451f7b1b79cd38b22c7026918ba7
kevintoomey <toomeykev@gmail.com>
2017-11-14 08:25:47 +0100

    ai imcomplte

---
+++ src/shared/ai/AI.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include "AI.h"
+
+#include <iostream>
+using namespace std;
+using namespace ai;
+using namespace engine;
+using namespace state;
+
+namespace ai{
+    void AI::listCommands (vector<unique_ptr<Command>>& list, State& state, int num){
+        
+    }
+    void AI::run (Engine& engine, int num) {
+        
+    }
+
+  
+};
---
+++ src/shared/ai/RandomAI.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+#include "RandomAI.h"
+
+#include <iostream>
+using namespace std;
+using namespace ai;
+
+namespace ai{
+    void RandomAI::run (engine::Engine& engine, int num){
+        
+    }
+    RandomAI::RandomAI (){
+              
+        
+    }
+
+  
+};
