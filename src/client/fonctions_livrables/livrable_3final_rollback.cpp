b27597e720430b984575ed7001d390ead485cd97
Chloé Lodolo <chloe.lodolo@gmail.com>
2017-12-06 10:31:05 +0100

    Première ébauche onction livrable rollback

--- src/client/fonctions_livrables/livrable_3final_rollback.cpp
+++ src/client/fonctions_livrables/livrable_3final_rollback.cpp
@@ -25,6 +25,100 @@ void livrable_3final_rollback(string commande)
     {
         cout<<"La commande est rollback"<<endl;
         
+        int count = 0;
+        
+        HeuristicAI* AIPlayer = new HeuristicAI();
+        Engine moteur;
+        State& etat = moteur.getState();
+                
+        if(moteur.getState().getPlayer()==DRAGONS)
+        {
+            cout<<"It is now the DRAGONS' turn !"<<endl;
+        }
+        else
+        {
+            cout<< "It is now the UNICORNS' turn !"<<endl;
+        }
+        cout<<"Appuyez sur la touche Entrée pour continuer"<<endl<<endl;
+        
+        // initialisation de l'état
+        InitBasicState* initState = new InitBasicState();
+        moteur.addCommand((Command*)initState);
+        moteur.update();
+
+        // on crée une couche à partir de l'ElementTabLayer qu'on a construit
+        ElementTabLayer Layer1(etat.getTerritoryBoard());
+        ElementTabLayer Layer2(etat.getTeamBoard());
+        StateLayer Layer3(etat);
+
+        // ouverture de la page du jeu
+        RenderWindow window(VideoMode(800,600,32),"Risk Fantasy | Unicorns VS Dragons",
+                Style::Close | Style::Titlebar);
+        
+        // on fait tourner le programme tant que la fenêtre n'est pas fermée
+        while (window.isOpen())
+        {
+            Event event;
+            while (window.pollEvent(event))
+            {
+                // fermeture de la fenêtre lorsque l'utilisateur le souhaite
+                if (event.type == Event::Closed)
+                {
+                    window.close();
+                }
+            }
+            
+            if(Keyboard::isKeyPressed(Keyboard::Return))
+            {
+                count += 1;
+                
+                cout<<"Fermez la fenêtre pour quitter"<<endl;
+                cout<<"Appuyez sur la touche Entrée pour continuer"<<endl<<endl;
+                
+                if (count == 1)
+                {
+                    moteur.undo();
+                }
+                if (moteur.getState().isGameOver()){
+                    string strfinal;
+                    if(moteur.getState().getPlayer()==DRAGONS)
+                    {
+                        strfinal="UNICORNS";
+                    }
+                    else
+                    {
+                        strfinal="DRAGONS";
+                    }
+
+                    cout<< "GAME OVER : The winner is "<<strfinal<<endl;
+                    window.close(); 
+                }
+                else
+                {
+                    actions = moteur.update();
+                    AIPlayer->run(moteur);
+                }
+                sleep(milliseconds(1000));
+            }
+            
+            // à chaque tour, on efface l'ancien rendu
+            window.clear(Color::Black);
+            
+            // on dessine la surface des territoires
+            Layer1.initSurface();
+            window.draw(*(Layer1.getSurface()));
+
+            // on dessine la surface des équipes
+            Layer2.initSurface();
+            window.draw(*(Layer2.getSurface()));
+            
+            // on dessine la surface des chiffres
+            Layer3.initSurface();
+            window.draw(*(Layer3.getSurface()));
+            
+            // et on affiche le nouveau rendu
+            window.display();
+        }
         
         
         
